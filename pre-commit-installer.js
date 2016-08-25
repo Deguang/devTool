12dbe535ca6cc4d07d41bc09d2fac854b962fbc2
lideguang <better_li@live.com>
2016-08-25 20:02:45 +0800

    update pre-commit-installer

--- pre-commit-installer.js
+++ pre-commit-installer.js
@@ -1,4 +1,23 @@
 var fs = require('fs'),
     exec = require('child_process').exec;
 
-exec('curl ');
+if(!fs.existsSync('.git/hooks/')) {
+    console.log('[INFO]: pre-commit installed failed.');
+    console.log('Please run this cmd on the root of project');
+}
+var download = exec('curl https://raw.githubusercontent.com/Deguang/devTool/master/pre-commit', (error, stdout, stderr) => {
+    if (error) {
+        console.log(error.stack);
+        console.log('`Error code: ' + error.code);
+    }
+
+    //写入文件
+    fs.writeFile('.git/hooks/pre-commit', stdout, {
+        mode: '0755'
+    }, function(err) {
+        if(err){
+            return console.log('[ERROR]:%s',err);
+        }
+        console.log('[INFO]: pre-commit installed successfully.');
+    })
+});
